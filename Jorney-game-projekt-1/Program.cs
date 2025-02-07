edfd4314db074454883d4fdcf1718adb6cf0b6f4
Signe Olofsson <signe.olofsson@elev.ga.ntig.se>
2025-02-07 19:37:36 +0100

    klar med Vi äventyr

--- Jorney-game-projekt-1/Program.cs
+++ Jorney-game-projekt-1/Program.cs
@@ -5,7 +5,7 @@ bool done = false;
 string name = "";
 while (!done)
 {
-    List<string> character = ["Jinx", "Vi", "Ekko", "Catlyn", "Vander"];
+    List<string> character = ["Jinx", "Vi", "Ekko", "Vander"];
 
     Console.WriteLine("Skriv ett namn från listan");
     foreach (var item in character)
@@ -13,7 +13,7 @@ while (!done)
         Console.WriteLine(item);
     }
 
-
+    Console.WriteLine();
 
     // Hämtar in namnet
     name = Console.ReadLine();
@@ -35,9 +35,10 @@ if (name == "Jinx")
 {
     Jinx();
 }
+//Om namnet är Vi
 else if (name == "Vi")
 {
-
+    Vi();
 }
 
 
@@ -91,8 +92,8 @@ static void Jinx()
     {
         yourWeaponHp = 20;
         villainName = "Ambessa";
-        
-    
+
+
     }
     else if (weaponname == "Pistol")
     {
@@ -113,10 +114,7 @@ static void Jinx()
         villainName = "Catlyn";
     }
 
-    FirstFight(weaponname, "Jinx", villainName,yourWeaponHp,Villain_hp);
-
-
-  
+    FirstFight(weaponname, "Jinx", villainName, yourWeaponHp, Villain_hp);
 
     Console.WriteLine("Yay du slog " + villainName);
 
@@ -149,10 +147,14 @@ static void Jinx()
         }
     }
 
+    Console.WriteLine();
+
+    //  rädda och skurkarna hitpoints 
     int Villain_hp2 = 460;
 
     string saved = "";
 
+    //Vilket håll man valde och vilken skurk och vem man ska rädda och ny vapen hitpoint 
     if (choosingDirection == "Höger")
     {
 
@@ -161,7 +163,7 @@ static void Jinx()
         Console.WriteLine($"När {villainName} fick tillbaka sin kraft tog hon {saved} tillfånga och nu utmanar hon dig till strid (hennes hit point har ökat +10 och ditt vapen har gångrat med 2)");
         yourWeaponHp = 40;
         weaponname = "Shockpistol";
-        
+
 
     }
     else if (choosingDirection == "Vänster")
@@ -188,29 +190,24 @@ static void Jinx()
     else if (choosingDirection == "VänsrerUpp")
     {
 
+        villainName = "Catlyn";
         saved = "Isha";
-        Console.WriteLine($"När {villain_name4} fick tillbaka sin kraft tog hon {saved} tillfånga och nu utmanar hon dig till strid (hennes hit point har ökat +10 och ditt vapen har gångrat med 2)");
+        Console.WriteLine($"När {villainName} fick tillbaka sin kraft tog hon {saved} tillfånga och nu utmanar hon dig till strid (hennes hit point har ökat +10 och ditt vapen har gångrat med 2)");
         yourWeaponHp = 300;
         weaponname = "Rocketlauncher";
-        villainName = villain_name4;
     }
 
-     SavingFight(saved,villainName,"Jinx",Villain_hp2,weaponname,yourWeaponHp); 
-
-
-
-
-    Console.WriteLine("yay du lyckades rädda " + saved);
-
-    Console.WriteLine("Du är klar med äventryret.");
+    SavingFight(saved, villainName, "Jinx", Villain_hp2, weaponname, yourWeaponHp);
 }
 
 static void Vi()
 {
+    // Vi spawnings punkt 
     Console.WriteLine("Du spawnar i the under city vid bron du börjar gå framåt till du möter din vän Babbete");
     Console.WriteLine("Hon säger att du ska välja ett av fyra vapen");
 
-    List<string> weapon = ["Atlas Gauntlets", "Blast Shield", "Golem Gan", "Pilbåge"];
+    // Vaperna Vi kan välja 
+    List<string> weapon = ["Atlas Gauntlets", "Blast Shield", "Pilbåge", "Golem Gan"];
     string armname;
 
     foreach (var item in weapon)
@@ -241,62 +238,135 @@ static void Vi()
 
 
     string Badguy = "";
-    int BadguyHp = 450;
-    int yourArmhp=0; 
-    
+    int BadguyHp = 550;
+    int yourArmhp = 0;
 
+    //Vilket vapen man väljer och dess hitpoints,Skurkensnamn också
     if (armname == "Atlas Goutlet")
     {
         Badguy = "Viktor";
-        yourArmhp=50; 
+        yourArmhp = 30;
+    }
+    else if (armname == "Blast Shield")
+    {
+        Badguy = "Ambessa";
+        yourArmhp = 50;
+    }
+    else if (armname == "Pilbåge")
+    {
+        Badguy = "Sevika";
+        yourArmhp = 100;
+    }
+    else if (armname == "Golem Gan")
+    {
+        Badguy = "Silco";
+        yourArmhp = 150;
     }
 
-    
+    FirstFight(armname, "Vi", Badguy, yourArmhp, BadguyHp);
 
-    FirstFight(armname, "Vi", Badguy,yourArmhp,BadguyHp);
-    
-    string taken= ""; 
-    int BadguyHp2=0; 
-    int yourArmhp2=0; 
+    Console.WriteLine("Du kommer till en korsning du kan välja mellan att gå");
 
-    SavingFight(taken,Badguy,"Vi",BadguyHp,armname,yourArmhp2); 
+    List<string> direktion = ["Höger", "Vänster", "Vänster Vänster", "Höger Höger"];
+    string chooseDirektion; 
+
+    foreach (var item in direktion)
+    {
+        Console.WriteLine(item); 
+    }
+    bool icecream=false; 
+
+    chooseDirektion=Console.ReadLine(); 
+
+    while (!icecream)
+    {
+        if (direktion.Contains(chooseDirektion))
+        {
+            Console.WriteLine("Du valde att gå åt"+chooseDirektion); 
+            icecream=true; 
+        }
+        else
+        {
+            Console.WriteLine("En rikting sa jag"); 
+        }
+    }
+
+    string taken = "";
+    int BadguyHp2 = 560;
+    int yourArmhp2 = 0;
+
+    if (chooseDirektion=="Höger")
+    {
+        taken="Loris";
+        Badguy="Viktor";
+        armname="Atlas Goutlet";
+        yourArmhp2=60;
+    }
+    else if (chooseDirektion=="Vänster")
+    {
+        taken="Jayce";
+        Badguy="Ambessa";
+        armname="Blast Shield";
+        yourArmhp2=100; 
+    }
+    else if (chooseDirektion=="Vänster Vänster")
+    {
+        taken="Catlyn"; 
+        Badguy="Sevika";
+        armname="Pilbåge";
+        yourArmhp2=200; 
+    }
+    else if (chooseDirektion=="Höger Höger")
+    {
+        taken="Powder";
+        Badguy="Silco";
+        armname="Golem Gan";
+        yourArmhp2=300; 
+    }
+
+    SavingFight(taken, Badguy, "Vi", BadguyHp2, armname, yourArmhp2);
 
 }
 
+// Alla komandon   för den första striden
 static void FirstFight(string weapon, string heroName, string bruteName, int weaponHp, int bruteHp)
 {
     Console.WriteLine($"{heroName} attackerar med sin {weapon} battles{bruteName} ");
 
-    while (bruteHp>0)
+    while (bruteHp > 0)
     {
-         Console.WriteLine("\n-----====Slå skurken ====----");
+        Console.WriteLine($"\n-----====Slå {bruteName} ====----");
         Console.WriteLine($"{heroName}:{weapon}: {weaponHp} {bruteName}: {bruteHp}\n");
 
-        int weaponDamage =weaponHp;
+        int weaponDamage = weaponHp;
         bruteHp -= weaponDamage;
         bruteHp = Math.Max(0, bruteHp);
         Console.WriteLine($"{weapon} gör {weaponDamage} skada på {bruteName}");
 
         Console.ReadKey();
     }
+    Console.WriteLine("YAY du slog" + bruteName);
 }
 
-static void SavingFight(string savingName,string bruteName,string heroName,int bruteHp2,string weapon,int weaponHp2)
+// Alla komandon för räddnings slagsmålet
+static void SavingFight(string savingName, string bruteName, string heroName, int bruteHp2, string weapon, int weaponHp2)
 {
-     Console.WriteLine($"{bruteName} tar {savingName} {heroName} måste rädda{savingName}och {bruteName} Hp adderas med 10 och{weapon} Hp gångras med 2 ");
+    Console.WriteLine($"{bruteName} tar {savingName} {heroName} måste rädda{savingName}och {bruteName} Hp adderas med 10 och{weapon} Hp gångras med 2 ");
 
-     while (bruteHp2>0)
-     {
-          Console.WriteLine("\n-----====Slå skurken ====----");
+    while (bruteHp2 > 0)
+    {
+        Console.WriteLine($"\n-----====Rädda{savingName} ====----");
         Console.WriteLine($"{heroName}:{weapon}: {weaponHp2} {bruteName}: {bruteHp2}\n");
 
-        int weaponDamage =weaponHp2;
+        int weaponDamage = weaponHp2;
         bruteHp2 -= weaponDamage;
-        bruteHp2 = Math.Max(0, bruteHp2);
+        bruteHp2 = Math.Max(0, bruteHp2);{}
         Console.WriteLine($"{weapon} gör {weaponDamage} skada på {bruteName}");
 
         Console.ReadKey();
-     }
+    }
+
+    Console.WriteLine($"YAY du lyckaderädda{savingName} och ditt äventyr är slut");
 
 }
 
